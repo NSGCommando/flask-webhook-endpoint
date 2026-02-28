d5f66d70f9ec61cc5ea7859d1df9937388ad979f
NSGCommando <41550763+NSGCommando@users.noreply.github.com>
2026-02-28 18:51:03 +0530

    Updated backend routing to insert action logs in required DB schema

--- app/webhook/routes.py
+++ app/webhook/routes.py
@@ -1,12 +1,60 @@
 from flask import Blueprint, jsonify, request
+from app.project_logger import project_logger # need to run from project root for absolute imports to work
+from app.extensions import mongo
 
 webhook = Blueprint('Webhook', __name__, url_prefix='/webhook')
 
 @webhook.route('/receiver', methods=["POST"])
 def receiver():
+    if mongo.db is None:
+        raise RuntimeError("Mongo DB has not been initialized with init_app(app)")
     data =request.get_json(silent=True) # use get_json() to handle possible empty responses
     if not data:
+        project_logger.warning("No event JSON received!")
         return jsonify({"error": "No JSON payload received"}), 400
     
-    print("Received webhook data:", data)
+    event = data.get("event_type")
+    merged_at = data.get("merged_at")
+    pr_action = data.get("pr_action")
+    author = data.get("author")
+    request_id = data.get("request_id")
+    received_timestamp = data.get("timestamp")
+    pr_from = data.get("pr_from")
+    pr_to = data.get("pr_to")
+    # compile all relevant data
+    log_doc={
+                "request_id": request_id,
+                "author": author,
+                "action": "",
+                "from_branch": pr_from,
+                "to_branch": pr_to,
+                "timestamp": received_timestamp
+            }
+
+    # Push event
+    if event == "push":
+        log_doc["action"] = "PUSH"
+        project_logger.info("[Push] Author: %s, UTC Timestamp: %s", author, received_timestamp)
+
+    # Pull request event
+    elif event == "pull_request":
+        # check for merged request or not
+        if merged_at:
+            log_doc["action"] = "MERGE"
+            project_logger.info(
+                "[Pull Request Merged] From: %s, To: %s, author: %s, Author: %s, Merge Time:%s",
+                pr_action,pr_from,pr_to,author,author,merged_at
+            )
+        else:
+            log_doc["action"] = "PULL_REQUEST"
+            project_logger.info(
+                "[Pull Request] From: %s, To: %s, author: %s",
+                pr_action,pr_from,pr_to,author
+            )
+    else:
+        project_logger.warning("[WARNING] Unkwown Request at time: %s",received_timestamp)
+
+    if event in ("push","pull_request"):
+        mongo.db["webhook_logs"].insert_one(log_doc)
+    project_logger.info("Received Request")
     return jsonify({"status": "received"}), 200
