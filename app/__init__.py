ef3d2f1d2a0d430e666eb5192196053ed8600096
NSGCommando <41550763+NSGCommando@users.noreply.github.com>
2026-02-28 18:41:57 +0530

    Moved app factory to __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask
-
+from flask_cors import CORS
+from app.extensions import init_my_app
 from app.webhook.routes import webhook
 
-
-# Creating our flask app
 def create_app():
-
     app = Flask(__name__)
-    
-    # registering all the blueprints
+    CORS(app)
+    init_my_app(app)
     app.register_blueprint(webhook)
-    
+    @app.route("/")
+    def index():
+        return "<h1>Hello, Flask + Cloudflared!</h1>"
     return app
